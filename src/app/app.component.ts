8292925867a913359d354143074e849c4eaabea8
Boubacar Barry <bouba@hackages.io>
2017-09-10 12:27:25 +0200

    Inline template and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+      `
+      .center-text {
+        text-align: center
+      }
+    `
+  ],
+  template: `
+    <!--Replacing the inline style by center class-->
+    <div class="center-text">
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+    </div>
+    <h2>Here are some links to help you start: </h2>
+    <ul>
+      <li>
+        <h2><a target="_blank" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" href="https://blog.angular.io//">Angular blog</a></h2>
+      </li>
+    </ul>
+
+  `,
 })
 export class AppComponent {
   title = 'app';
